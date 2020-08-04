44626363ee38f7e27f0ab0d06eb9a13877dcf2b4
Douglas <douglas.lisboa@edu.unipar.br>
2020-08-04 08:34:59 -0300

    Adicionado estilização na landing page.

--- web/src/pages/Landing/index.tsx
+++ web/src/pages/Landing/index.tsx
@@ -2,28 +2,32 @@ import React from 'react';
 
 import logo from '../../assets/images/logo.svg';
 import landingImg from '../../assets/images/landing.svg';
+
 import studyIcon from '../../assets/images/icons/study.svg'
 import giveClassesIcon from '../../assets/images/icons/give-classes.svg';
 import purpleHeartIcon from '../../assets/images/icons/purple-heart.svg';
+
+import './styles.css';
+
 function Landing() {
   return (
-    <div id="page-loading">
-      <div id="page-loading-content">
+    <div id="page-landing">
+      <div id="page-landing-content">
         <div className="logo-container">
           <img src={logo} alt=""/>
           <h2>Sua plataforma de estudos online.</h2>
         </div>
 
-        <img src={landingImg} alt='Plataforma de estudos' className='hero-image'/>
+        <img src={landingImg} alt='Plataforma de estudos' className="hero-image"/>
 
         <div className="buttons-container">
           <a href="" className="study">
-            <img src={studyIcon} alt=""/>
+            <img src={studyIcon} alt="Estudar"/>
             Estudar
           </a>
           <a href="" className="give-classes">
-            <img src={giveClassesIcon} alt=""/>
-            Estudar
+            <img src={giveClassesIcon} alt="Dar aulas"/>
+            Dar aulas
           </a>
         </div>
 
