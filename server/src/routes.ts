0a101e297cc21778af6754232721c1201abed541
Douglas <douglas.lisboa@edu.unipar.br>
2020-08-07 11:45:29 -0300

    Implementado nova estrutura para routes e controllers.

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,55 +1,13 @@
-import express from 'express';
-import db from './database/connection';
-import convertHourToMinutes from './utils/convertHourTominutes';
+import express, { response } from 'express';
+import ClassController from './controllers/ClassesController';
 
-const routes = express();
+const routes = express.Router();
+const classesControllers =  new ClassController();
 
-interface ScheduleDTO {
-  week_day: string;
-  from: string;
-  to: string;
-}
+routes.get('/classes', classesControllers.index);
+routes.post('/classes', classesControllers.create);
 
-routes.post('/classes', async (request, response) => {
-  const {
-    name,
-    avatar,
-    whatsapp,
-    bio,
-    subject,
-    cost,
-    schedule
-  } = request.body;
-
-  const insertUserIds = await db('users').insert({
-    name,
-    avatar,
-    whatsapp,
-    bio,
-  });
-
-  const user_id = insertUserIds[0];
-
-  const insertClassesId = await db('classes').insert({
-    subject,
-    cost,
-    user_id,
-  });
-
-  const class_id = insertClassesId[0];
-
-  const classSchedule = schedule.map((scheduleDTO: ScheduleDTO) => {
-    return {
-      week_day: scheduleDTO.week_day,
-      from: convertHourToMinutes(scheduleDTO.from),
-      to: convertHourToMinutes(scheduleDTO.to),
-      class_id
-    }
-  })
-
-  await db('class_schedule').insert(classSchedule);
-
-  return response.send();
-});
+routes.get('/classes', classesControllers.index);
+routes.post('/classes', classesControllers.create);
 
 export default routes;
