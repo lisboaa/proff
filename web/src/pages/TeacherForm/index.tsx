966f8307c31fe464274728d70ad75e0acbf471a4
Douglas <douglas.lisboa@edu.unipar.br>
2020-08-09 09:38:59 -0300

    Adicionado Select(options) de atulas.

--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 
 import PageHeader from '../../components/PageHeader';
-
+import Textarea from '../../components/Textarea';
 import Input from '../../components/Input';
+import Select from '../../components/Select';
 
 import warningIcon from '../../assets/images/icons/warning.svg';
 
@@ -22,11 +23,27 @@ function TeacherForm() {
               <Input name="name" label="Nome completo"/>
               <Input name="avatar" label="Avatar"/>
               <Input name="whatsapp" label="Whatsapp"/>
+              <Textarea name="bio" label="Biografia"/>
           </fieldset>
 
           <fieldset>
             <legend>Sobre a aula</legend>
-              <Input name="subject" label="Matéria"/>
+              <Select
+                name="subject"
+                label="Matéria"
+                options={[
+                  { value: 'Artes', label: 'Artes' },
+                  { value: 'Biologia', label: 'Biologia' },
+                  { value: 'Ciência', label: 'Ciência' },
+                  { value: 'Educação Fisica', label: 'Educação Fisica' },
+                  { value: 'Física', label: 'Física' },
+                  { value: 'Geografia', label: 'Geografia' },
+                  { value: 'História', label: 'História' },
+                  { value: 'Geografica', label: 'Geografica' },
+                  { value: 'Matemática', label: 'Matemática' },
+                  { value: 'Português', label: 'Português' },
+                  { value: 'Química', label: 'Química' },
+                ]}/>
               <Input name="cost" label="Custo da sua hora por aula"/>
           </fieldset>
 
